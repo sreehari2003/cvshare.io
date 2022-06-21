609218072357199d1a7c5f6f29b16585f1aedacd
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-06-22 02:02:19 +0530

    feat: added basic prisma functions

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -3,6 +3,7 @@ import appError from "../utils/appError";
 import catchAsync from "../utils/catchAsync";
 import { prisma } from "../server/index";
 import { Request, Response, NextFunction } from "express";
+import { hashJwt } from "../utils/hash";
 
 export const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -10,6 +11,20 @@ export const createUser = catchAsync(
     if (!name || !email || !username || !image) {
       return next(new appError("missing all required inputs", 404));
     }
+    const existUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (existUser) {
+      const token = hashJwt(existUser.UID);
+      res.cookie("jwtID", token);
+      res
+        .status(201)
+        .json(serverResponse(`welcome back ${existUser.name}`, existUser));
+    }
+
     const result = await prisma.user.create({
       data: {
         name,
@@ -19,7 +34,8 @@ export const createUser = catchAsync(
         UID,
       },
     });
-
+    const token = hashJwt(UID);
+    res.cookie("jwtID", token);
     res.json(serverResponse("user was created", result));
   }
 );
