36ce695092fc6cec37036e3b2d9c714c68145a36
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-07-03 00:37:19 +0530

    BREAKING CHANGE: mui -> Chakra UI

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react'
 
 import React, { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp;
