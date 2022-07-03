136e9c3022df3a7e46dd1c19c595224d50a7a898
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-07-03 19:45:20 +0530

    feat: added auth ui

--- frontend/firebase/config.ts
+++ frontend/firebase/config.ts
@@ -1,12 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAKEqifx1Madi2-88MDgMWfl8Og8xUHDs0",
   authDomain: "finance-flutter.firebaseapp.com",
@@ -19,5 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
