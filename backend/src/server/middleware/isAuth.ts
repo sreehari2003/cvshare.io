19859968e6e5d0ad888764b223343c50fdaa8d01
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-07-21 23:41:08 +0530

    chore: authorisation

--- backend/src/server/middleware/isAuth.ts
+++ backend/src/server/middleware/isAuth.ts
@@ -44,3 +44,38 @@ export const isAuth = catchAsync(
     }
   }
 );
+
+// export const isCompanyAuth = catchAsync(
+//   async (req: User, _res: Response, next: NextFunction) => {
+//     let token: string;
+//     if (
+//       req.headers &&
+//       req.headers.authorization &&
+//       req.headers.authorization.startsWith("Bearer")
+//     ) {
+//       token = req.headers.authorization.split(" ")[1];
+//     } else {
+//       return next(new AppError("user not logged in", 401));
+//     }
+//     const { _id } = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+//     if (!_id) {
+//       return next(new AppError("Inavild user id please login Again", 401));
+//     }
+
+//     const extistingUser = await prisma.company.findUnique({
+//       where: {
+//         id: _id,
+//       },
+//       select: {
+//         email: true,
+//       },
+//     });
+//     if (extistingUser) {
+//       // This might throw an TS error
+
+//       next();
+//     } else {
+//       return next(new AppError("Inavild user id please login Again", 401));
+//     }
+//   }
+// );
--- frontend/context/Auth.tsx
+++ frontend/context/Auth.tsx
@@ -3,53 +3,51 @@ import { api } from "../api/index";
 import Cookies from "js-cookie";
 
 interface AuthCtx {
-    token: string | null;
-    isLoggedIn: boolean;
-    logOut: () => void;
-    user: any;
+  token: string | null;
+  isLoggedIn: boolean;
+  logOut: () => void;
+  user: any;
 }
 const AuthContext = React.createContext<AuthCtx>({
-    token: null,
-    isLoggedIn: false,
-    logOut: () => { },
-    user: null,
+  token: null,
+  isLoggedIn: false,
+  logOut: () => {},
+  user: null,
 });
 
 interface Child {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const AuthContextProvider = ({ children }: Child) => {
-    const [token, setToken] = useState(null);
-    const [user, setUser] = useState<any>(null);
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState<any>(null);
 
-    useEffect(() => {
-        async function loadUserFromCookies() {
-            const token = Cookies.get('jwtID')
-            if (token) {
-                api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-                const { data: user } = await api.get('auth/user');
-                if (user) setUser(user);
-            }
-            // setLoading(false)
-        }
-        loadUserFromCookies()
-    }, [])
-    const userIsLoggedIn = !!token;
-    const LogOutHandler = () => {
-        setToken(null);
-        window.location.reload();
-        setUser(null);
-    };
-    const contextValue = {
-        token,
-        isLoggedIn: userIsLoggedIn,
-        logOut: LogOutHandler,
-        user
-    };
-    return (
-        <AuthContext.Provider value={contextValue}>
-            {children}
-        </AuthContext.Provider>
-    );
+  useEffect(() => {
+    async function loadUserFromCookies() {
+      const token = Cookies.get("jwtID");
+      if (token) {
+        api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        const { data: user } = await api.get("auth/me");
+        if (user) setUser(user);
+      }
+      // setLoading(false)
+    }
+    loadUserFromCookies();
+  }, []);
+  const userIsLoggedIn = !!token;
+  const LogOutHandler = () => {
+    setToken(null);
+    window.location.reload();
+    setUser(null);
+  };
+  const contextValue = {
+    token,
+    isLoggedIn: userIsLoggedIn,
+    logOut: LogOutHandler,
+    user,
+  };
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
 };
